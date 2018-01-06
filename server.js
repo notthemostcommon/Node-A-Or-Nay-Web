a27f5a9a3e587b319315ee3b1858e50381e51d98
James Smith <noletubby77@hotmail.com>
2018-01-05 20:58:56 -0500

    server.js file created

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+const router = require('express').Router();
+const PORT = process.env.PORT || 3000;
+
+require('dotenv').config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(morgan('dev'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set ('view engine', 'ejs');
+
+
+// app.get('/', (req, res) => {
+//   res.render('index')
+// });
+
+
+const restController = require('./controllers/rest-controller');
+
+// app.get('/', restController.index);
+app.get('/',restController.search); 
+// router.post('/search', controller.search)
+// app.use('/weather', require('./routes/weather-routes'))
+
+app.get('*', (req, res) => {
+  res.status(404).json({
+    message: 'Look elsewhere as this path only leads to danger'
+  })
+})
+
+  app.listen(PORT, () =>{
+    console.log(`listening on port ${PORT}`)
+  })
