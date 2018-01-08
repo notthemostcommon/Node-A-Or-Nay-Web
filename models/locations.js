17179e58df48a7a9317a30e567ab2861cae5c124
James Smith <noletubby77@hotmail.com>
2018-01-08 06:37:51 -0500

    updates to models

---
+++ models/locations.js
@@ -0,0 +1,16 @@
+const db = require('../db/config'); 
+
+const Locations = {}; 
+
+Locations.create = location => {
+	return db.one(
+	`
+		INSERT INTO locations 
+		(camis, dba)
+		VALUES ($1, $2) RETURNING *
+		`, 
+		[location.camis, location.dba]
+		); 
+}; 
+
+module.exports = Locations; 
